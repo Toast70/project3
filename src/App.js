e3fa025dcb50f5f09c43e9986b25be4b474f13f4
Toast70 <77930220+Toast70@users.noreply.github.com>
2021-05-21 00:11:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Display from "./components/Display";
 function App() {
   return (
-    <div className="container bg-white mt-3">
+    <div className="container-fluid bg-white mt-3">
       <Display/>
     </div>
   );
